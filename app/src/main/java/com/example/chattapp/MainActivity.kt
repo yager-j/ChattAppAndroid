c4b7ff82aaba30468f283724057282386dd54aa2
LucaSalmi <luca.salmi90@gmail.com>
2022-04-01 10:51:36 +0200

    added firebase

--- app/src/main/java/com/example/chattapp/MainActivity.kt
+++ app/src/main/java/com/example/chattapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.chattapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.chattapp.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,34 @@ class MainActivity : AppCompatActivity() {
         binder = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binder.root)
+
+        val db = Firebase.firestore
+        val user = hashMapOf(
+
+            "first" to "Luca",
+            "last" to "Salmi",
+            "born" to "1990"
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
