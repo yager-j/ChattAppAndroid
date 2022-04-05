62db0c0a1ffcb83b549bc20c46c06d92ed459381
Oscar Karlsson <oscar.karlsson@student.kyh.se>
2022-04-05 12:01:53 +0200

    Login settings

--- app/src/main/java/com/example/chattapp/MainActivity.kt
+++ app/src/main/java/com/example/chattapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chattapp.databinding.ActivityMainBinding
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 private lateinit var binder: ActivityMainBinding
 
@@ -13,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         binder = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binder.root)
 
+        Realm.init(this)
+        val config = RealmConfiguration.Builder()
+            .name("chatAppDB")
+            .allowWritesOnUiThread(true)
+            .schemaVersion(1)
+            .build()
+        Realm.setDefaultConfiguration(config)
     }
 }
