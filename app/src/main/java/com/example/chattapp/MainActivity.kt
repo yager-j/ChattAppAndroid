c7157e57ebe45f59092c99361effd08a4706a5ba
LucaSalmi <luca.salmi90@gmail.com>
2022-04-05 10:31:02 +0200

    adapter init

--- app/src/main/java/com/example/chattapp/MainActivity.kt
+++ app/src/main/java/com/example/chattapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.chattapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chattapp.databinding.ActivityMainBinding
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
 private lateinit var binder: ActivityMainBinding
+private lateinit var userDao: UserDao
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,11 @@ class MainActivity : AppCompatActivity() {
             .build()
         Realm.setDefaultConfiguration(config)
 
+        userDao = UserDao()
 
+        binder.chatsList.layoutManager = LinearLayoutManager(this)
+        val adapter = MyAdapter(userDao.getUsers()){ position -> onListItemClick(position) }
+        binder.chatsList.adapter = adapter
 
     }
 }
