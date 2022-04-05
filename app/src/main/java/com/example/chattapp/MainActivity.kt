65b40f2c549928662ffdc428e5a4011dc333bf53
LucaSalmi <luca.salmi90@gmail.com>
2022-04-05 09:38:09 +0200

    added view binding

--- app/src/main/java/com/example/chattapp/MainActivity.kt
+++ app/src/main/java/com/example/chattapp/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.chattapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chattapp.databinding.ActivityMainBinding
 import io.realm.Realm
+
+private lateinit var binder: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
 
     }
 }
