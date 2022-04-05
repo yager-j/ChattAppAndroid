093aee021363f64e7a7f73a15b8d4d3ebbb07271
Luca Salmi <luca.salmi90@gmail.com>
2022-04-05 09:19:18 +0200

    Revert "first day tests"

--- app/src/main/java/com/example/chattapp/MainActivity.kt
+++ app/src/main/java/com/example/chattapp/MainActivity.kt
@@ -2,16 +2,10 @@ package com.example.chattapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.chattapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binder : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binder = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binder.root)
+        setContentView(R.layout.activity_main)
     }
 }
